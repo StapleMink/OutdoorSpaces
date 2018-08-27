07a9d542ee420ea4537bfb2b4e4776c149be3b26
StapleMink <daniel.budziwojski@gmail.com>
2018-08-26 19:17:10 -0700

    Version 0: See if this works.

--- OutdoorSpaces/ViewController.swift
+++ OutdoorSpaces/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Make an awesome app work!
     }
 
     override func didReceiveMemoryWarning() {
