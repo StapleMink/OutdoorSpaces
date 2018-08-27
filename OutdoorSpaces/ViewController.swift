ba850da34bdd18baa3923272c1455fdeb2b84232
Cynthia Hom <cynthia@cynthias-macbook-air.local>
2018-08-26 19:20:29 -0700

    first commit

--- OutdoorSpaces/ViewController.swift
+++ OutdoorSpaces/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
+        //hello! 
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // Make an awesome app work!
