823d62a607dc04d2e5f918048b2c32170050d1bf
Cynthia Hom <cynthia@cynthias-air.attlocal.net>
2018-09-15 20:06:28 -0700

    made table view cell file

---
+++ OutdoorSpaces/ParkTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ParkTableViewCell.swift
+//  OutdoorSpaces
+//
+//  Created by Cynthia Hom on 9/15/18.
+//  Copyright © 2018 Sandbox Apps. All rights reserved.
+//
+
+import UIKit
+
+class ParkTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
